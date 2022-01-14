478e6f0f7f05d542b23fab1976d3889059bb9043
Abdelmenam Tarek <75137269+Abdelmenam-Tarek-Abdelmenam@users.noreply.github.com>
2022-01-14 13:45:41 +0200

    Update scrape.py

--- pythonScripts/scrape.py
+++ pythonScripts/scrape.py
@@ -1,40 +1,71 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import sqlite3
 import time
- 
- # https://dalilaldwaa.com/
 
-df = pd.DataFrame(columns=['name' , "price" , "pic" , "details"])
+df = pd.DataFrame(columns=['name' , "price" , "pic" , "details" , "marketName" , "scientificName" ])
 
-errorList = []
-for i in range(1,2271) :
-    try :
-        url = 'https://dalilaldwaa.com/medicine-list?page={}'.format(i)
-        r = requests.get(url)
-        html_doc = r.text
-        soup = BeautifulSoup(html_doc , features="html.parser")
-        all_items = soup.find_all("div", {"class": "row cm-item"})
-        print(i)
-    except :
-        print("error in {}".format(i))
-        errorList.append(i)
-        time.sleep(2)
-        df.to_csv("data.csv", encoding='utf-8' ,index=False)
-        continue 
+startTime=time.time()
+
+url = 'https://dalilaldwaa.com/medicine-list?'
+r = requests.get(url)
+html_doc = r.text
+soup = BeautifulSoup(html_doc , features="html.parser")
+all_items = soup.find_all("a", {"class": "page-link"})
+pagesNum = int(all_items[-2].text) +1
+print("number of pages in the site {}".format(pagesNum-1))
+
+for i in range(1,pagesNum) :
+    while True :
+        try :
+            url = 'https://dalilaldwaa.com/medicine-list?page={}'.format(i)
+            r = requests.get(url)
+            html_doc = r.text
+            soup = BeautifulSoup(html_doc , features="html.parser")
+            all_items = soup.find_all("div", {"class": "row cm-item"})
+            print(i)
+            break
+        except :
+            print("error in {}".format(i))
+            time.sleep(2)
+            df.to_csv("data.csv", encoding='utf-8' ,index=False)
+            continue 
 
     for item in all_items : 
-        try : 
-            main = item.find("img")
-            name = main["alt"]
-            img = main["src"]
-            price = item.find("p", {"class": "color-gray"}).text.strip()
-            details = item.find("a", {"class": "cm-details"})['href'].strip()
-            df2 = {'name': name , 'price': price, 'pic': img , "details" : details}
-            df = df.append(df2, ignore_index = True)
-        except :
-            print("err")
-            df.to_csv("data.csv", encoding='utf-8' ,index=False)
+        while True :
+            try : 
+                main = item.find("img")
+                name = main["alt"]
+                img = main["src"]
+                price = item.find("p", {"class": "color-gray"}).text.strip()
+                details = item.find("a", {"class": "cm-details"})['href'].strip()
+                
+                detailsLink = "https://dalilaldwaa.com/" + str(details)
+                r = requests.get(detailsLink)
+                html_doc = r.text
+                soup = BeautifulSoup(html_doc , features="html.parser")
+                marketName = (soup.find("div" , {"class" : "even"}).text).replace("الإسم التجاري" , "").strip()
+                scieceName = (soup.find_all("div" , {"class" : "odd"})[1].text).replace("الإسم العلمي" , "").strip()
+
+                des = (soup.find("div" , {"class" : "tab-pane fade show active"}).text)
+                df2 = {'name': name , 'price': price, 'pic': img , "details" : des , "marketName":marketName , "scientificName":scieceName }
+
+                df = df.append(df2, ignore_index = True)
+                break
+            except :
+                print("err")
+                df.to_csv("data.csv", encoding='utf-8' ,index=False)
+                time.sleep(2)
+
+endTime=time.time()
+print("Taked time {}".format(endTime-startTime))
+
+print('the data has {} drug'.format(df.shape[0]))
+print("data has {} duplicate drug".format(df.duplicated().sum()))
+print(df.info())
+
+conn = sqlite3.connect('data.db')
+df.to_sql('data', conn, if_exists='replace', index=False)
 
 df.to_csv("data.csv", encoding='utf-8' ,index=False)
-print(errorList)
