13befa589c05ed2ecf2163fb7addb2e811e7137b
ahmed khaled <ahmedkhaledibrahem@gmail.com>
2022-02-01 00:08:21 +0200

    main_tab search

--- lib/screens/admin_screens/main_tab.dart
+++ lib/screens/admin_screens/main_tab.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:pharmacyapp/contsants/const_colors.dart';
 import 'package:pharmacyapp/contsants/widgets.dart';
 import 'package:pharmacyapp/reusable/components.dart';
@@ -37,7 +39,8 @@ List<String> mostWanted = [
                 backgroundColor: MaterialStateProperty.all(themeColor),
               ),
               onPressed: () {
-                navigateTo(context, SearchResultsScreen(), true);
+                showSearch(context: context, delegate: CustomSearchDelegate());
+                //navigateTo(context, SearchResultsScreen(), true);
               },
               child: SizedBox(
                 width: double.infinity,
@@ -254,4 +257,174 @@ List<String> mostWanted = [
       ),
     );
   }
+}
+
+class CustomSearchDelegate extends SearchDelegate{
+  List<String> items = ['one', 'two','first','second','ahmed','apple','orange'];
+
+  List<Map<String,String>> drugItem = [
+    {
+      'name':'Panadol Extra 50mg',
+      'price':'20',
+      'image':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJohJOH9MzlKeMhnxD4urqQxz5AXb_qgE-Xg&usqp=CAU'
+    },
+
+    {
+      'name':'Panadol Extra 50mg',
+      'price':'20',
+      'image':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJohJOH9MzlKeMhnxD4urqQxz5AXb_qgE-Xg&usqp=CAU'
+    },
+
+    {
+      'name':'Panadol Extra 50mg',
+      'price':'20',
+      'image':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJohJOH9MzlKeMhnxD4urqQxz5AXb_qgE-Xg&usqp=CAU'
+    },
+    {
+      'name':'Panadol Extra 50mg',
+      'price':'20',
+      'image':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJohJOH9MzlKeMhnxD4urqQxz5AXb_qgE-Xg&usqp=CAU'
+    },
+  ];
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: (){query = '';},
+        icon: const Icon(Icons.clear_rounded),)
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(onPressed: (){close(context, null);},
+      icon: const Icon(Icons.arrow_back),);
+
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+
+    //SQL selection Here
+    List matchQuary = items;
+    //--------------------
+
+   return ListView.builder(
+     itemCount: matchQuary.length,
+       itemBuilder: (BuildContext context, int index){
+         return ListTile(title: Text(matchQuary[index]),);}
+   );
+
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    //SQL selection Here
+    List matchQuary = items;
+    //--------------------
+    return GridView.builder(
+      scrollDirection: Axis.vertical,
+      physics: defaultScrollPhysics,
+      shrinkWrap: true,
+      padding: const EdgeInsets.all(12.0),
+      itemCount: drugItem.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          childAspectRatio: MediaQuery.of(context).size.width / (520),
+          crossAxisCount: 2,
+          crossAxisSpacing: 3,
+          mainAxisSpacing: 5),
+      itemBuilder: (BuildContext context, int index) => searchItem(context,index),
+    );
+  }
+
+  Widget searchItem(BuildContext context,int index) {
+    return Card(
+      elevation: 10,
+      color: Colors.blue,
+      child: Column(
+        children: [
+          defaultSpaceH(3),
+          Stack(
+            alignment: Alignment.topLeft,
+            children: [
+              SizedBox(
+                  width: MediaQuery.of(context).size.width / 2.4,
+                  height: 120,
+                  child: CachedNetworkImage(
+                    width: 80,
+                    height: 80,
+                    fit: BoxFit.cover,
+                    placeholder: (context, url) => const CircularProgressIndicator(),
+                    errorWidget: (context, url, error) =>
+                        Center(
+                          child: Text(
+                            drugItem[index]['name']!.length < 2 ? " "
+                                : drugItem[index]['name']!.trim().substring(0, 2),
+                            style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                    imageUrl: drugItem[index]['image']!)),
+
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                SingleChildScrollView(
+                  child: Tooltip(
+                    message: drugItem[index]['name']!,
+                    child: Text(
+                      drugItem[index]['name']!,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(color: Colors.black, fontSize: 18),
+                    ),
+                  ),
+                  scrollDirection: Axis.vertical,
+                ),
+                const SizedBox(
+                  height: 3,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 90,
+                      child: SingleChildScrollView(
+                        child: Text(
+                          "${drugItem[index]['price']!} LE",
+                          style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w800,
+                              color: Colors.white),
+                        ),
+                        scrollDirection: Axis.horizontal,
+                      ),
+                    ),
+                    const Spacer(),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: themeColor,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: IconButton(
+                          color: Colors.white,
+                          onPressed: () {
+                           // navigateTo(context, SearchResultsScreen(), true);
+                          },
+                          icon: const Icon(Icons.edit)),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
 }
